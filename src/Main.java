801e4d5b7d04fe4c6031e9bd7a428d247dbf1fb1
Roman Kravchenko <roman.me@mail.ru>
2020-05-01 13:06:32 +0300

    bonus

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int amount = 1100;
+        int bonus = 0;
+        if (amount > 1000) {
+
+            bonus = (amount - amount % 100) / 100;}
+            int total = balance + amount + bonus;
+
+            System.out.println(total);
+
+
+
+
+    }
+}
